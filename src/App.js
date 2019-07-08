d2482d9fe15ad94238ae6f53ab687b229679447d
vladmog <vladmog@gmail.com>
2019-07-08 13:40:42 -0700

    Made scores dynamic by adding increment functionality to buttons

--- src/App.js
+++ src/App.js
@@ -30,11 +30,15 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
+          <button 
+            onClick = {() => setHomeScore(homeScore + 1)}
+            className="homeButtons__touchdown">Home Touchdown</button>
           <button className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
+          <button 
+            onClick = {() => setAwayScore(awayScore + 1)}
+            className="awayButtons__touchdown">Away Touchdown</button>
           <button className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
